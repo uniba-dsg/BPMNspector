6c2d5b03c7911490fb8494634a72b052a911bb0a
Philipp Neugebauer <ppn@nexgo.de>
2014-03-24 17:31:11 +0100

    error printout added

--- src/de/uniba/dsg/ppn/ba/SchematronBPMNValidator.java
+++ src/de/uniba/dsg/ppn/ba/SchematronBPMNValidator.java
@@ -5,12 +5,17 @@ import java.io.File;
 
 import javax.xml.transform.stream.StreamSource;
 
+import org.oclc.purl.dsdl.svrl.FailedAssert;
+import org.oclc.purl.dsdl.svrl.SchematronOutputType;
+
 import com.phloc.schematron.ISchematronResource;
 import com.phloc.schematron.pure.SchematronResourcePure;
 
 import de.uniba.dsg.ppn.ba.xml.XmlReader;
 
 public class SchematronBPMNValidator {
+
+	private static StringBuffer error = new StringBuffer();
 
 	public static boolean validateViaPureSchematron(String xml)
 			throws Exception {
@@ -20,16 +25,34 @@ public class SchematronBPMNValidator {
 		if (!schematronSchema.isValidSchematron()) {
 			throw new IllegalArgumentException("Invalid Schematron!");
 		}
-		return schematronSchema.getSchematronValidity(
+
+		boolean valid = schematronSchema.getSchematronValidity(
 				new StreamSource(
 						new ByteArrayInputStream(xml.getBytes("UTF-8"))))
 				.isValid();
+
+		if (!valid) {
+			SchematronOutputType sot = schematronSchema
+					.applySchematronValidationToSVRL(new StreamSource(
+							new ByteArrayInputStream(xml.getBytes("UTF-8"))));
+
+			for (int i = 0; i < sot
+					.getActivePatternAndFiredRuleAndFailedAssertCount(); i++) {
+				if (sot.getActivePatternAndFiredRuleAndFailedAssertAtIndex(i) instanceof FailedAssert) {
+					FailedAssert f = (FailedAssert) sot
+							.getActivePatternAndFiredRuleAndFailedAssertAtIndex(i);
+					error.append(f.getLocation() + ": " + f.getText() + "\r\n");
+				}
+			}
+		}
+
+		return valid;
 	}
 
 	public static void main(String[] args) throws Exception {
-		// File f = new File("E:\\Philipp\\BA\\testprocesses\\101\\fail.bpmn");
-		File f = new File(
-				"E:\\Philipp\\BA\\dump\\bpmn-by-example\\eMail Voting\\Email Voting 2.bpmn");
+		File f = new File("E:\\Philipp\\BA\\testprocesses\\101\\fail.bpmn");
+		// File f = new File(
+		// "E:\\Philipp\\BA\\dump\\bpmn-by-example\\eMail Voting\\Email Voting 2.bpmn");
 		// File f = new
 		// File("E:\\Philipp\\BA\\testprocesses\\101\\success.bpmn");
 
@@ -38,5 +61,8 @@ public class SchematronBPMNValidator {
 
 		boolean check = SchematronBPMNValidator.validateViaPureSchematron(text);
 		System.out.println("Is File " + f.getName() + " valid? " + check);
+		if (!check) {
+			System.out.println(error);
+		}
 	}
 }
