6287728e43db201121bde43a85814610801c8daf
Philipp Neugebauer <ppn@nexgo.de>
2014-03-24 10:48:52 +0100

    no file creation after xml manipulation anymore, so more efficient

--- src/de/uniba/dsg/ppn/ba/SchematronBPMNValidator.java
+++ src/de/uniba/dsg/ppn/ba/SchematronBPMNValidator.java
@@ -1,7 +1,7 @@
 package de.uniba.dsg.ppn.ba;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.util.List;
 
 import javax.xml.transform.stream.StreamSource;
 
@@ -9,11 +9,10 @@ import com.phloc.schematron.ISchematronResource;
 import com.phloc.schematron.pure.SchematronResourcePure;
 
 import de.uniba.dsg.ppn.ba.xml.XmlReader;
-import de.uniba.dsg.ppn.ba.xml.XmlWriter;
 
 public class SchematronBPMNValidator {
 
-	public static boolean validateViaPureSchematron(File xmlFile)
+	public static boolean validateViaPureSchematron(String xml)
 			throws Exception {
 		final ISchematronResource schematronSchema = SchematronResourcePure
 				.fromFile(SchematronBPMNValidator.class.getResource(
@@ -21,22 +20,19 @@ public class SchematronBPMNValidator {
 		if (!schematronSchema.isValidSchematron()) {
 			throw new IllegalArgumentException("Invalid Schematron!");
 		}
-		return schematronSchema
-				.getSchematronValidity(new StreamSource(xmlFile)).isValid();
+		return schematronSchema.getSchematronValidity(
+				new StreamSource(
+						new ByteArrayInputStream(xml.getBytes("UTF-8"))))
+				.isValid();
 	}
 
 	public static void main(String[] args) throws Exception {
 		File f = new File("E:\\Philipp\\BA\\testprocesses\\101\\fail.bpmn");
 
 		XmlReader r = new XmlReader();
-		List<String> lineList = r.readXmlFile(f);
+		String text = r.readXmlFile(f);
 
-		f = new File("temp.xml");
-
-		XmlWriter w = new XmlWriter();
-		w.writeXml(f, lineList);
-
-		boolean check = SchematronBPMNValidator.validateViaPureSchematron(f);
+		boolean check = SchematronBPMNValidator.validateViaPureSchematron(text);
 		System.out.println("File is valid? " + check);
 	}
 }
