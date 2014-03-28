1ae6c857ad04ec3062f731d3a74ed0db4d0fa14a
Philipp Neugebauer <ppn@nexgo.de>
2014-03-28 19:35:57 +0100

    tests for ext.100 constraint check

---
+++ test/de/uniba/dsg/ppn/ba/Ext100.java
@@ -0,0 +1,40 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext100 {
+
+	@Test
+	public void testConstraintEventFail() throws Exception {
+		File f = new File(
+				"D:\\Philipp\\BA\\testprocesses\\100\\fail_event.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:subProcess/bpmn:startEvent | bpmn:transaction/bpmn:startEvent | bpmn:adHocSubProcess/bpmn:startEvent[0]: No EventDefinition is allowed for Start Events in Sub-Process definitions");
+	}
+
+	@Test
+	public void testConstraintEventRefFail() throws Exception {
+		File f = new File(
+				"D:\\Philipp\\BA\\testprocesses\\100\\fail_event_ref.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:subProcess/bpmn:startEvent | bpmn:transaction/bpmn:startEvent | bpmn:adHocSubProcess/bpmn:startEvent[0]: No EventDefinition is allowed for Start Events in Sub-Process definitions");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File("D:\\Philipp\\BA\\testprocesses\\100\\success.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, true);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
