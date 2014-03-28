008514d2522360e974e6b511fdb844a070496d6e
Philipp Neugebauer <ppn@nexgo.de>
2014-03-28 23:10:48 +0100

    ext.106 tests done

---
+++ test/de/uniba/dsg/ppn/ba/Ext106.java
@@ -0,0 +1,40 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext106 {
+
+	@Test
+	public void testConstraintEventFail() throws Exception {
+		File f = new File(
+				"D:\\Philipp\\BA\\testprocesses\\106\\fail_cancel_end_event.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:cancelEventDefinition[0]: A cancel EndEvent is only allowed in a transaction sub-process");
+	}
+
+	@Test
+	public void testConstraintEventRefFail() throws Exception {
+		File f = new File(
+				"D:\\Philipp\\BA\\testprocesses\\106\\fail_sub_process.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:cancelEventDefinition[0]: A cancel EndEvent is only allowed in a transaction sub-process");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File("D:\\Philipp\\BA\\testprocesses\\106\\success.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, true);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
