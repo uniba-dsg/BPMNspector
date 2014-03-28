91ede99f3b69ecfb567229e8445f562dae9d1bff
Philipp Neugebauer <ppn@nexgo.de>
2014-03-28 23:15:41 +0100

    test for ext.108 done

---
+++ test/de/uniba/dsg/ppn/ba/Ext108.java
@@ -0,0 +1,20 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext108 {
+
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File("D:\\Philipp\\BA\\testprocesses\\108\\Fail.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:messageFlow[@targetRef][0]: An End Event MUST NOT be a target for a message flow");
+	}
+}
