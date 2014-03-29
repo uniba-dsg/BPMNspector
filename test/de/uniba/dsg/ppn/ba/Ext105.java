2315cca3ee0308163112d5558c2db287936f40d6
Philipp Neugebauer <ppn@nexgo.de>
2014-03-30 00:55:18 +0100

    constraint check for ext.105 and tests done

---
+++ test/de/uniba/dsg/ppn/ba/Ext105.java
@@ -0,0 +1,50 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext105 {
+
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath()
+				+ "105\\fail_end_without_sub-events.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:startEvent[0]: An end event must be present when a start event is used in the same process level");
+	}
+
+	@Test
+	public void testConstraintSubFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath()
+				+ "105\\fail_with_sub-startevent.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:startEvent[1]: An end event must be present when a start event is used in the same process level");
+	}
+
+	@Test
+	public void testConstraintSubSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath()
+				+ "105\\success_with_sub-events.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, true);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath()
+				+ "105\\success_without_sub-events.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, true);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
