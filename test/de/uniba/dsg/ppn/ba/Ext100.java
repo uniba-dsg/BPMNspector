dddfbfab293e107b44de143678ff6f0d335047c1
Philipp Neugebauer <ppn@nexgo.de>
2014-03-29 23:44:28 +0100

    added test case for ext.100 for schematron check

--- test/de/uniba/dsg/ppn/ba/Ext100.java
+++ test/de/uniba/dsg/ppn/ba/Ext100.java
@@ -19,6 +19,17 @@ public class Ext100 {
 	}
 
 	@Test
+	public void testConstraintTransactionEventFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath()
+				+ "100\\fail_event_transaction.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:subProcess/bpmn:startEvent | bpmn:transaction/bpmn:startEvent | bpmn:adHocSubProcess/bpmn:startEvent[0]: No EventDefinition is allowed for Start Events in Sub-Process definitions");
+	}
+
+	@Test
 	public void testConstraintEventRefFail() throws Exception {
 		File f = new File(TestHelper.getTestFilePath()
 				+ "100\\fail_event_ref.bpmn");
