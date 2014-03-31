07fb04ef26d0642b73ccd2b2e704fad18b7ee2b6
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 21:54:49 +0200

    validation and test case for ext.095 done

---
+++ test/de/uniba/dsg/ppn/ba/Ext095.java
@@ -0,0 +1,45 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext095 {
+
+	// TODO: validate other elements
+
+	// @Test
+	// public void testConstraintFail() throws Exception {
+	// File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
+	// + "fail.bpmn");
+	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+	// assertFalse(valid);
+	// assertEquals(
+	// SchematronBPMNValidator.getErrors(),
+	// "//bpmn:dataOutput[0]: A DataOutput must be referenced by at least one OutputSet");
+	// }
+
+	@Test
+	public void testConstraintEndFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
+				+ "fail_end.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:endEvent/bpmn:messageEventDefinition[0]: EventDefinitions defined in a throw event are only valid within this element");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
+				+ "success.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertTrue(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
