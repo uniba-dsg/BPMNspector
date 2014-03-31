546496c3ba39fcaf2b1ac7e0d8315934dca87f98
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 22:56:38 +0200

    ext.109 tests and constraint check done

---
+++ test/de/uniba/dsg/ppn/ba/Ext109.java
@@ -0,0 +1,41 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext109 {
+
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "109" + File.separator
+				+ "fail.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:endEvent[@id = string(//bpmn:messageFlow/@sourceRef)][0]: If an end event is source of a MessageFlow definition, at least one messageEventDefinition must be present");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "109" + File.separator
+				+ "success.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertTrue(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+
+	@Test
+	public void testConstraintRefSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "109" + File.separator
+				+ "success_ref.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertTrue(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
