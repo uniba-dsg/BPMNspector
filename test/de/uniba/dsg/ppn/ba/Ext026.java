6e06d141f15af6fef4df9a85a503bbacd820cd19
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 16:18:54 +0200

    better test commenting out

--- test/de/uniba/dsg/ppn/ba/Ext026.java
+++ test/de/uniba/dsg/ppn/ba/Ext026.java
@@ -11,23 +11,23 @@ public class Ext026 {
 	// TODO: test all elements
 	// TODO: add error messages assert
 
-	@Test
-	public void testConstraintActivityFail() throws Exception {
-		File f = new File(TestHelper.getTestFilePath() + "026" + File.separator
-				+ "fail_activity.bpmn");
-		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-		assertEquals(valid, false);
-		// assertEquals(SchematronBPMNValidator.getErrors(), "");
-	}
-
 	// @Test
-	// public void testConstraintGatewayFail() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath()
-	// + "026" + File.separator + "fail_gateway.bpmn");
+	// public void testConstraintActivityFail() throws Exception {
+	// File f = new File(TestHelper.getTestFilePath() + "026" + File.separator
+	// + "fail_activity.bpmn");
 	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
 	// assertEquals(valid, false);
 	// assertEquals(SchematronBPMNValidator.getErrors(), "");
 	// }
+
+	@Test
+	public void testConstraintGatewayFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "026" + File.separator
+				+ "fail_gateway.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		// assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
 
 	@Test
 	public void testConstraintSuccess() throws Exception {
