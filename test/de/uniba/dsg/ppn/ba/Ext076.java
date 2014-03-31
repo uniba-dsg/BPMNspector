3300e54ebeae23457a44ee8d4dbfdc8fedc3b049
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 16:56:25 +0200

    commented out tests of ext.076 are now used again

--- test/de/uniba/dsg/ppn/ba/Ext076.java
+++ test/de/uniba/dsg/ppn/ba/Ext076.java
@@ -1,6 +1,7 @@
 package de.uniba.dsg.ppn.ba;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -11,38 +12,38 @@ public class Ext076 {
 
 	// TODO: constraint validation
 
-	// @Test
-	// public void testConstraintFail1() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath() + "076" + File.separator +
-	// "fail_1.bpmn");
-	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-	// assertFalse(valid);
-	// assertEquals(
-	// SchematronBPMNValidator.getErrors(),
-	// "//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
-	// }
-	//
-	// @Test
-	// public void testConstraintFail2() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath() + "076" + File.separator +
-	// "fail_2.bpmn");
-	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-	// assertFalse(valid);
-	// assertEquals(
-	// SchematronBPMNValidator.getErrors(),
-	// "//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
-	// }
-	//
-	// @Test
-	// public void testConstraintFail3() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath() + "076" + File.separator +
-	// "fail_3.bpmn");
-	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-	// assertFalse(valid);
-	// assertEquals(
-	// SchematronBPMNValidator.getErrors(),
-	// "//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
-	// }
+	@Test
+	public void testConstraintFail1() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "076" + File.separator
+				+ "fail_1.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
+	}
+
+	@Test
+	public void testConstraintFail2() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "076" + File.separator
+				+ "fail_2.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
+	}
+
+	@Test
+	public void testConstraintFail3() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "076" + File.separator
+				+ "fail_3.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:dataObjectReference[@name][0]: Naming Convention: name = Data Object Name [Data Object Reference State]");
+	}
 
 	@Test
 	public void testConstraintSuccess() throws Exception {
