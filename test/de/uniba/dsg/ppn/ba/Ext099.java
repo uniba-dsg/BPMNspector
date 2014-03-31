19e0ca77bd7c4fbcdf09729fa93b1c5102301fbc
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 11:45:49 +0200

    created test for imported process to test ext.099, #38

--- test/de/uniba/dsg/ppn/ba/Ext099.java
+++ test/de/uniba/dsg/ppn/ba/Ext099.java
@@ -8,8 +8,6 @@ import org.junit.Test;
 
 public class Ext099 {
 
-	// TODO: missing test case: fail_call_ref_process.bpmn
-
 	@Test
 	public void testConstraintEventFail() throws Exception {
 		File f = new File(TestHelper.getTestFilePath() + "099" + File.separator
@@ -25,6 +23,17 @@ public class Ext099 {
 	public void testConstraintEventRefFail() throws Exception {
 		File f = new File(TestHelper.getTestFilePath() + "099" + File.separator
 				+ "fail_eventref.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertEquals(valid, false);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:process[./@id = string(//bpmn:callActivity/@calledElement)][0]: Referenced process must have at least one None Start Event");
+	}
+
+	@Test
+	public void testConstraintImportedProcessFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "099" + File.separator
+				+ "fail_call_ref_process.bpmn");
 		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
 		assertEquals(valid, false);
 		assertEquals(
