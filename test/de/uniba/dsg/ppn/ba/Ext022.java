748e4dd27a4abca37a25c6d1235795069da1632e
Philipp Neugebauer <ppn@nexgo.de>
2014-03-31 21:04:54 +0200

    tests and constraint check for ext.022 done

---
+++ test/de/uniba/dsg/ppn/ba/Ext022.java
@@ -0,0 +1,52 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext022 {
+
+	// TODO: validate all allowed arguments
+
+	@Test
+	public void testConstraintEventSubProcessFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "022" + File.separator
+				+ "fail_event_sub_process.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]: For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target");
+	}
+
+	// @Test
+	// public void testConstraintEventsSuccess() throws Exception {
+	// File f = new File(TestHelper.getTestFilePath() + "022" + File.separator
+	// + "success_events.bpmn");
+	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+	// assertTrue(valid);
+	// assertEquals(SchematronBPMNValidator.getErrors(), "");
+	// }
+	//
+	// @Test
+	// public void testConstraintGatewaysSuccess() throws Exception {
+	// File f = new File(TestHelper.getTestFilePath() + "022" + File.separator
+	// + "success_gateways.bpmn");
+	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+	// assertTrue(valid);
+	// assertEquals(SchematronBPMNValidator.getErrors(), "");
+	// }
+
+	@Test
+	public void testConstraintTasksSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "022" + File.separator
+				+ "success_tasks.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertTrue(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
