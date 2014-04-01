497d50eee2ab81659d4dea6b8a3a5bdb98c78fdb
Philipp Neugebauer <ppn@nexgo.de>
2014-04-01 17:08:38 +0200

    tests for ext.031 done

--- test/de/uniba/dsg/ppn/ba/Ext031.java
+++ test/de/uniba/dsg/ppn/ba/Ext031.java
@@ -1,6 +1,7 @@
 package de.uniba.dsg.ppn.ba;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -9,7 +10,49 @@ import org.junit.Test;
 
 public class Ext031 {
 
-	// TODO: Insert the other tests ;-)
+	@Test
+	public void testConstraintCircleFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "031" + File.separator
+				+ "Fail_circle.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:messageFlow[0]: A message flow must connect ’InteractionNodes’ from different Pools");
+	}
+
+	@Test
+	public void testConstraintFromPoolFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "031" + File.separator
+				+ "Fail_message_flow_from_pool.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:messageFlow[0]: A message flow must connect ’InteractionNodes’ from different Pools\r\n//bpmn:messageFlow[@targetRef][0]: An End Event MUST NOT be a target for a message flow");
+	}
+
+	@Test
+	public void testConstraintToPoolFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "031" + File.separator
+				+ "Fail_message_flow_to_pool.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:messageFlow[0]: A message flow must connect ’InteractionNodes’ from different Pools\r\n//bpmn:messageFlow[@sourceRef][0]: A Start Event MUST NOT be a source for a message flow");
+	}
+
+	@Test
+	public void testConstraintSamePoolFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "031" + File.separator
+				+ "Fail_message_flow_in_same_pool.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:messageFlow[0]: A message flow must connect ’InteractionNodes’ from different Pools\r\n//bpmn:messageFlow[@sourceRef][0]: A Start Event MUST NOT be a source for a message flow\r\n//bpmn:messageFlow[@targetRef][0]: An End Event MUST NOT be a target for a message flow");
+	}
 
 	@Test
 	public void testConstraintSuccess() throws Exception {
