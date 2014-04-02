11b47317810896e941cfd9b8ab8d0403d826d047
Philipp Neugebauer <ppn@nexgo.de>
2014-04-02 21:44:20 +0200

    all elements affected by ext.095 are now validated and all tests are used

--- test/de/uniba/dsg/ppn/ba/Ext095.java
+++ test/de/uniba/dsg/ppn/ba/Ext095.java
@@ -10,18 +10,16 @@ import org.junit.Test;
 
 public class Ext095 {
 
-	// FIXME: validate other elements
-
-	// @Test
-	// public void testConstraintFail() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
-	// + "Fail.bpmn");
-	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-	// assertFalse(valid);
-	// assertEquals(
-	// SchematronBPMNValidator.getErrors(),
-	// "//bpmn:dataOutput[0]: A DataOutput must be referenced by at least one OutputSet");
-	// }
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
+				+ "Fail.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(
+				SchematronBPMNValidator.getErrors(),
+				"//bpmn:intermediateThrowEvent/bpmn:messageEventDefinition[0]: EventDefinitions defined in a throw event are only valid within this element");
+	}
 
 	@Test
 	public void testConstraintEndFail() throws Exception {
