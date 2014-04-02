a7136e54423b6d683e4d16b30606d21083475b63
Philipp Neugebauer <ppn@nexgo.de>
2014-04-02 23:35:24 +0200

    ext.002 tests done

---
+++ test/de/uniba/dsg/ppn/ba/Ext002.java
@@ -0,0 +1,31 @@
+package de.uniba.dsg.ppn.ba;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class Ext002 {
+
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "001" + File.separator
+				+ "Fail.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(),
+				"One imported file does not exist");
+	}
+
+	@Test
+	public void testConstraintSuccess() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "001" + File.separator
+				+ "Success.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertTrue(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(), "");
+	}
+}
