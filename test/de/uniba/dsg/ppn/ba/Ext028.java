4c2969836ea7b9b94441cea56bece905c37a8fc2
Philipp Neugebauer <ppn@nexgo.de>
2014-04-02 03:49:22 +0200

    all ext.028 tests are now used again

--- test/de/uniba/dsg/ppn/ba/Ext028.java
+++ test/de/uniba/dsg/ppn/ba/Ext028.java
@@ -1,6 +1,7 @@
 package de.uniba.dsg.ppn.ba;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -9,17 +10,15 @@ import org.junit.Test;
 
 public class Ext028 {
 
-	// TODO: constraint validation
-
-	// @Test
-	// public void testConstraintFail() throws Exception {
-	// File f = new File(TestHelper.getTestFilePath() + "028" + File.separator
-	// + "Fail.bpmn");
-	// boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
-	// assertFalse(valid);
-	// assertEquals(SchematronBPMNValidator.getErrors(),
-	// "//bpmn:dataInput[0]: A Sequence Flow must not cross the border of a Pool");
-	// }
+	@Test
+	public void testConstraintFail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "028" + File.separator
+				+ "Fail.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(),
+				"//bpmn:sequenceFlow[0]: A Sequence Flow must not cross the border of a Pool");
+	}
 
 	@Test
 	public void testConstraintSuccess() throws Exception {
