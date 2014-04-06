4031a5b95bfa8d6ff36fb1855bd72564d5730eda
Philipp Neugebauer <ppn@nexgo.de>
2014-04-06 22:40:19 +0200

    test cases for ext.002 implemented and test cases for ext.001 removed from ext.002 test file

--- test/de/uniba/dsg/ppn/ba/Ext002.java
+++ test/de/uniba/dsg/ppn/ba/Ext002.java
@@ -11,19 +11,29 @@ import org.junit.Test;
 public class Ext002 {
 
 	@Test
-	public void testConstraintFail() throws Exception {
-		File f = new File(TestHelper.getTestFilePath() + "001" + File.separator
-				+ "Fail.bpmn");
+	public void testConstraintImport1Fail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "002" + File.separator
+				+ "fail_import.bpmn");
 		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
 		assertFalse(valid);
 		assertEquals(SchematronBPMNValidator.getErrors(),
-				"One imported file does not exist");
+				"Files have double ids");
+	}
+
+	@Test
+	public void testConstraintImport2Fail() throws Exception {
+		File f = new File(TestHelper.getTestFilePath() + "002" + File.separator
+				+ "fail_import2.bpmn");
+		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
+		assertFalse(valid);
+		assertEquals(SchematronBPMNValidator.getErrors(),
+				"Files have double ids");
 	}
 
 	@Test
 	public void testConstraintSuccess() throws Exception {
-		File f = new File(TestHelper.getTestFilePath() + "001" + File.separator
-				+ "Success.bpmn");
+		File f = new File(TestHelper.getTestFilePath() + "002" + File.separator
+				+ "success_import.bpmn");
 		boolean valid = SchematronBPMNValidator.validateViaPureSchematron(f);
 		assertTrue(valid);
 		assertEquals(SchematronBPMNValidator.getErrors(), "");
