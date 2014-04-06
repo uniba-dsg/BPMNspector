c61766c6921d81afbd1140ce2b875d846f5a5960
Philipp Neugebauer <ppn@nexgo.de>
2014-04-07 00:13:16 +0200

    bugfix for violation.java

--- src/de/uniba/dsg/bpmn/Violation.java
+++ src/de/uniba/dsg/bpmn/Violation.java
@@ -7,16 +7,19 @@ import javax.xml.bind.annotation.XmlType;
 /**
  * Basic class representing a single Violation of a BPMN constraint.
  * 
- * Each {@code Violation} is defined by four attributes to describe the violation clearly:
+ * Each {@code Violation} is defined by four attributes to describe the
+ * violation clearly:
  * 
  * <ul>
- * 	<li>constraint - key of the violated constraint (e.g. EXT.032, REF.100,...)</li>
- *  <li>fileName - the name of the affected file</li>
- *  <li>line - the line number in the file where the violation occured</li>
- *  <li>xPath - an XPath expression which can be used to navigate to the affected element.</li>
+ * <li>constraint - key of the violated constraint (e.g. EXT.032, REF.100,...)</li>
+ * <li>fileName - the name of the affected file</li>
+ * <li>line - the line number in the file where the violation occured</li>
+ * <li>xPath - an XPath expression which can be used to navigate to the affected
+ * element.</li>
  * </ul>
  * 
- * Moreover, the attribute message can be used to provide a description of the violation.
+ * Moreover, the attribute message can be used to provide a description of the
+ * violation.
  */
 @XmlType(namespace = "http://www.uniba.de/pi/bpmn-cons/validation")
 public class Violation {
@@ -26,28 +29,33 @@ public class Violation {
 
 	@XmlAttribute(required = true)
 	private String fileName;
-	
+
 	@XmlAttribute
 	private int line;
-	
+
 	@XmlElement(namespace = "http://www.uniba.de/pi/bpmn-cons/validation")
 	private String xPath;
-	
+
 	@XmlElement(namespace = "http://www.uniba.de/pi/bpmn-cons/validation")
 	private String message;
 
 	public Violation() {
-		
+
 	}
-	
+
 	/**
-	 * Constructor 
+	 * Constructor
 	 * 
-	 * @param constraint id of the failed constraint
-	 * @param fileName name of the affected file
-	 * @param line line number
-	 * @param xPath XPath expression to the affected element/attribute
-	 * @param message description of the violation
+	 * @param constraint
+	 *            id of the failed constraint
+	 * @param fileName
+	 *            name of the affected file
+	 * @param line
+	 *            line number
+	 * @param xPath
+	 *            XPath expression to the affected element/attribute
+	 * @param message
+	 *            description of the violation
 	 */
 	public Violation(String constraint, String fileName, int line,
 			String xPath, String message) {
@@ -85,23 +93,24 @@ public class Violation {
 	public String getxPath() {
 		return xPath;
 	}
-	
+
 	/**
 	 * @return message to describe the violation
 	 */
 	public String getMessage() {
 		return message;
 	}
-	
+
 	@Override
 	public String toString() {
-		String str = "["+fileName+", line "+line+"]: Violation of "+constraint;
-	
-		if(message!=null && message.equals("")) {
-			str += ": "+message;
+		String str = "[" + fileName + ", line " + line + "]: Violation of "
+				+ constraint;
+
+		if (message != null && !message.equals("")) {
+			str += ": " + message;
 		}
-		if(xPath!=null && xPath.equals("")) {
-			str +="(XPath: "+xPath+")";
+		if (xPath != null && !xPath.equals("")) {
+			str += "(XPath: " + xPath + ")";
 		}
 		return str;
 	}
