eb97aad6813b4e41fab396b1de7851f8c8cd25d6
Philipp Neugebauer <ppn@nexgo.de>
2014-04-14 18:19:27 +0200

    TestMain adapted to validator changes

--- src/de/uniba/dsg/ppn/ba/TestMain.java
+++ src/de/uniba/dsg/ppn/ba/TestMain.java
@@ -2,18 +2,20 @@ package de.uniba.dsg.ppn.ba;
 
 import java.io.File;
 
+import de.uniba.dsg.bpmn.ValidationResult;
+
 public class TestMain {
 
 	public static void main(String[] args) throws Exception {
 		File f = new File(TestHelper.getTestFilePath() + "preprocessing"
 				+ File.separator + "fail_call_ref_process_call.bpmn");
 		SchematronBPMNValidator validator = new SchematronBPMNValidator();
-		boolean check = validator.validate(f);
-		System.out.println("Is File " + f.getName() + " valid? " + check);
+		ValidationResult result = validator.validate(f);
+		System.out.println("Is File " + f.getName() + " valid? "
+				+ result.isValid());
 
-		if (!check) {
-			Helper.printViolations(validator.getValidationResult()
-					.getViolations());
+		if (!result.isValid()) {
+			Helper.printViolations(result.getViolations());
 		}
 
 	}
