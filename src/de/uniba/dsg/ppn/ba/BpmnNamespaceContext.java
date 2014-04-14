45fdde4716c956b50417267eb6f954ae4f659e8d
Philipp Neugebauer <ppn@nexgo.de>
2014-04-14 16:54:57 +0200

    BPMNNamespaceContext class created for reducing redundancy

---
+++ src/de/uniba/dsg/ppn/ba/BpmnNamespaceContext.java
@@ -0,0 +1,30 @@
+package de.uniba.dsg.ppn.ba;
+
+import java.util.Iterator;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+
+public class BpmnNamespaceContext implements NamespaceContext {
+
+	@Override
+	public String getNamespaceURI(String prefix) {
+		if ("bpmn".equals(prefix)) {
+			return SchematronBPMNValidator.bpmnNamespace;
+		} else if ("xml".equals(prefix)) {
+			return XMLConstants.XML_NS_URI;
+		}
+		return XMLConstants.NULL_NS_URI;
+	}
+
+	@Override
+	public String getPrefix(String uri) {
+		return "";
+	}
+
+	@Override
+	public Iterator<?> getPrefixes(String uri) {
+		return null;
+	}
+
+}
