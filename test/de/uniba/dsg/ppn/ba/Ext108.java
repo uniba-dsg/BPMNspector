fafa0e5e4cef64d0e569edbe962183a756e2c33e
Philipp Neugebauer <ppn@nexgo.de>
2014-04-15 00:31:56 +0200

    ext.108 tests adapted

--- test/de/uniba/dsg/ppn/ba/Ext108.java
+++ test/de/uniba/dsg/ppn/ba/Ext108.java
@@ -9,6 +9,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.uniba.dsg.bpmn.ValidationResult;
+import de.uniba.dsg.bpmn.Violation;
+
 public class Ext108 {
 
 	SchematronBPMNValidator validator = null;
@@ -29,8 +32,17 @@ public class Ext108 {
 				+ "Fail.bpmn");
 		ValidationResult result = validator.validate(f);
 		assertFalse(result.isValid());
+		assertEquals(2, result.getViolations().size());
+		Violation v = result.getViolations().get(0);
 		assertEquals(
-				validator.getErrors(),
-				"//bpmn:messageFlow[0]: A message flow must connect ’InteractionNodes’ from different Pools\r\n//bpmn:messageFlow[@targetRef][0]: An End Event MUST NOT be a target for a message flow");
+				"A message flow must connect ’InteractionNodes’ from different Pools",
+				v.getMessage());
+		assertEquals("//bpmn:messageFlow[0]", v.getxPath());
+		assertEquals(7, v.getLine());
+		v = result.getViolations().get(1);
+		assertEquals("An End Event MUST NOT be a target for a message flow",
+				v.getMessage());
+		assertEquals("//bpmn:messageFlow[@targetRef][0]", v.getxPath());
+		assertEquals(7, v.getLine());
 	}
 }
