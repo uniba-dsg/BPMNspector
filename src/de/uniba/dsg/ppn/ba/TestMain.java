fed4738cdfd8c2948fed92204a3b2dde16ccf294
Philipp Neugebauer <ppn@nexgo.de>
2014-04-17 02:46:35 +0200

    test main revised

--- src/de/uniba/dsg/ppn/ba/TestMain.java
+++ src/de/uniba/dsg/ppn/ba/TestMain.java
@@ -2,14 +2,19 @@ package de.uniba.dsg.ppn.ba;
 
 import java.io.File;
 
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
 import de.uniba.dsg.bpmn.ValidationResult;
 import de.uniba.dsg.ppn.ba.helper.Helper;
 
 public class TestMain {
 
 	public static void main(String[] args) throws Exception {
-		File f = new File(TestHelper.getTestFilePath() + "095" + File.separator
-				+ "fail_end.bpmn");
+		Logger logger = (Logger) LoggerFactory.getLogger("BpmnValidator");
+		// logger.setLevel(Level.DEBUG);
+		File f = new File(TestHelper.getTestFilePath() + "002" + File.separator
+				+ "success_import.bpmn");
 		SchematronBPMNValidator validator = new SchematronBPMNValidator();
 		ValidationResult result = validator.validate(f);
 		System.out.println("Is File " + f.getName() + " valid? "
