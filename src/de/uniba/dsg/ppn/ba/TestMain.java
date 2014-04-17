f354c89363186fb197bd06ea8fe95aaa339fe6b2
Philipp Neugebauer <ppn@nexgo.de>
2014-04-17 14:18:06 +0200

    test main revised

--- src/de/uniba/dsg/ppn/ba/TestMain.java
+++ src/de/uniba/dsg/ppn/ba/TestMain.java
@@ -2,17 +2,12 @@ package de.uniba.dsg.ppn.ba;
 
 import java.io.File;
 
-import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.Logger;
 import de.uniba.dsg.bpmn.ValidationResult;
 import de.uniba.dsg.ppn.ba.helper.Helper;
 
 public class TestMain {
 
 	public static void main(String[] args) throws Exception {
-		Logger logger = (Logger) LoggerFactory.getLogger("BpmnValidator");
-		// logger.setLevel(Level.DEBUG);
 		File f = new File(TestHelper.getTestFilePath() + "002" + File.separator
 				+ "success_import.bpmn");
 		SchematronBPMNValidator validator = new SchematronBPMNValidator();
