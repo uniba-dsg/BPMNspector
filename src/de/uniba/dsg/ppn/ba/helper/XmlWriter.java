b20bf764d2a3865a5938c17cff744edcbc0c4539
Philipp Neugebauer <ppn@nexgo.de>
2014-04-17 14:17:27 +0200

    xml writer revised and improved

--- src/de/uniba/dsg/ppn/ba/helper/XmlWriter.java
+++ src/de/uniba/dsg/ppn/ba/helper/XmlWriter.java
@@ -6,17 +6,30 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
 import de.uniba.dsg.bpmn.ValidationResult;
 
 public class XmlWriter {
 
+	private JAXBContext context;
+	private Marshaller marshaller;
+	private Logger logger;
+
+	public XmlWriter() {
+		logger = (Logger) LoggerFactory.getLogger("BpmnValidator");
+		try {
+			context = JAXBContext.newInstance(ValidationResult.class);
+			marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		} catch (JAXBException e) {
+			logger.error("jaxb writer initialization failed: {}", e.getCause());
+		}
+	}
+
 	public void writeResult(ValidationResult result, File file)
 			throws JAXBException {
-
-		JAXBContext context = JAXBContext.newInstance(ValidationResult.class);
-		Marshaller marshaller = context.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
 		marshaller.marshal(result, file);
 	}
 
