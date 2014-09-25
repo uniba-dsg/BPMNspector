acd28071a6a2585822d21a7e79de7c252d4cfd1a
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2014-09-25 14:54:56 +0200

    Moving testprocesses into project structure

--- src/test/java/de/uniba/dsg/ppn/ba/TestHelper.java
+++ src/test/java/de/uniba/dsg/ppn/ba/TestHelper.java
@@ -5,8 +5,6 @@ import java.io.File;
 public class TestHelper {
 
 	public static String getTestFilePath() {
-		String path = TestHelper.class.getResource("").toString();
-		return path.substring(5, path.indexOf("schematron") - 1)
-				+ File.separator + "testprocesses" + File.separator;
+		return "bin" + File.separator;
 	}
 }
