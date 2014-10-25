0e683a7073417dd91f0c8ca3d4a028dd157838bb
Philipp Neugebauer <ppn@nexgo.de>
2014-10-25 20:09:54 +0200

    ext.056 test code improved

--- src/test/java/de/uniba/dsg/ppn/ba/Ext056.java
+++ src/test/java/de/uniba/dsg/ppn/ba/Ext056.java
@@ -17,7 +17,10 @@ import de.uniba.dsg.ppn.ba.validation.SchematronBPMNValidator;
 
 public class Ext056 {
 
-    SchematronBPMNValidator validator = null;
+    private SchematronBPMNValidator validator = null;
+    private final static String ERRORMESSAGEONE = "For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target";
+    private final static String ERRORMESSAGETWO = "A SubProcess must not contain Choreography Activities";
+    private final static String XPATHSTRING = "//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]";
 
     @Before
     public void setUp() {
@@ -39,16 +42,11 @@ public class Ext056 {
         assertFalse(result.isValid());
         assertEquals(2, result.getViolations().size());
         Violation v = result.getViolations().get(0);
-        assertEquals(
-                "For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target",
-                v.getMessage());
-        assertEquals(
-                "//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]",
-                v.getxPath());
+        assertEquals(ERRORMESSAGEONE, v.getMessage());
+        assertEquals(XPATHSTRING, v.getxPath());
         assertEquals(11, v.getLine());
         v = result.getViolations().get(1);
-        assertEquals("A SubProcess must not contain Choreography Activities",
-                v.getMessage());
+        assertEquals(ERRORMESSAGETWO, v.getMessage());
         assertEquals("//bpmn:subProcess[0]", v.getxPath());
         assertEquals(4, v.getLine());
     }
@@ -62,16 +60,11 @@ public class Ext056 {
         assertFalse(result.isValid());
         assertEquals(2, result.getViolations().size());
         Violation v = result.getViolations().get(0);
-        assertEquals(
-                "For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target",
-                v.getMessage());
-        assertEquals(
-                "//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]",
-                v.getxPath());
+        assertEquals(ERRORMESSAGEONE, v.getMessage());
+        assertEquals(XPATHSTRING, v.getxPath());
         assertEquals(11, v.getLine());
         v = result.getViolations().get(1);
-        assertEquals("A SubProcess must not contain Choreography Activities",
-                v.getMessage());
+        assertEquals(ERRORMESSAGETWO, v.getMessage());
         assertEquals("//bpmn:subProcess[0]", v.getxPath());
         assertEquals(4, v.getLine());
     }
@@ -85,16 +78,11 @@ public class Ext056 {
         assertFalse(result.isValid());
         assertEquals(2, result.getViolations().size());
         Violation v = result.getViolations().get(0);
-        assertEquals(
-                "For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target",
-                v.getMessage());
-        assertEquals(
-                "//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]",
-                v.getxPath());
+        assertEquals(ERRORMESSAGEONE, v.getMessage());
+        assertEquals(XPATHSTRING, v.getxPath());
         assertEquals(11, v.getLine());
         v = result.getViolations().get(1);
-        assertEquals("A SubProcess must not contain Choreography Activities",
-                v.getMessage());
+        assertEquals(ERRORMESSAGETWO, v.getMessage());
         assertEquals("//bpmn:transaction[0]", v.getxPath());
         assertEquals(4, v.getLine());
     }
@@ -108,16 +96,11 @@ public class Ext056 {
         assertFalse(result.isValid());
         assertEquals(2, result.getViolations().size());
         Violation v = result.getViolations().get(0);
-        assertEquals(
-                "For a Process: Of the types of FlowNode, only Activities, Gateways, and Events can be the target. However, Activities that are Event SubProcesses are not allowed to be a target",
-                v.getMessage());
-        assertEquals(
-                "//bpmn:*[./@id = string(//bpmn:sequenceFlow/@targetRef)][0]",
-                v.getxPath());
+        assertEquals(ERRORMESSAGEONE, v.getMessage());
+        assertEquals(XPATHSTRING, v.getxPath());
         assertEquals(11, v.getLine());
         v = result.getViolations().get(1);
-        assertEquals("A SubProcess must not contain Choreography Activities",
-                v.getMessage());
+        assertEquals(ERRORMESSAGETWO, v.getMessage());
         assertEquals("//bpmn:subProcess[0]", v.getxPath());
         assertEquals(4, v.getLine());
     }
