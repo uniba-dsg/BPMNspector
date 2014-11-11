73148f75993a4377a9895a389e1ae68afe86c9b1
Philipp Neugebauer <ppn@nexgo.de>
2014-11-11 21:13:57 +0100

    ext.146 + ext.150 test code improved

--- src/test/java/de/uniba/dsg/ppn/ba/Ext146.java
+++ src/test/java/de/uniba/dsg/ppn/ba/Ext146.java
@@ -1,115 +1,78 @@
 package de.uniba.dsg.ppn.ba;
 
-import ch.qos.logback.classic.Level;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import de.uniba.dsg.bpmn.ValidationResult;
 import de.uniba.dsg.bpmn.Violation;
 import de.uniba.dsg.ppn.ba.helper.BpmnValidationException;
-import de.uniba.dsg.ppn.ba.validation.SchematronBPMNValidator;
-import org.junit.Before;
-import org.junit.Test;
 
-import java.io.File;
+public class Ext146 extends TestCase {
 
-import static org.junit.Assert.*;
-
-public class Ext146 {
-
-    private SchematronBPMNValidator validator;
     private final static String ERRORMESSAGE = "Only messageEventDefininitions, escalationEventDefinitions, errorEventDefinitions, cancelEventDefinitions, compensationEventDefinitions, signalEventDefinitions and terminateEventDefinitions are allowed for end events";
     private final static String XPATHSTRING = "//bpmn:endEvent[0]";
 
-    @Before
-    public void setUp() {
-        validator = new SchematronBPMNValidator();
-        validator.setLogLevel(Level.OFF);
-    }
-
     @Test
     public void testConstraintLinkFail() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "146" + File.separator
-                + "fail_link.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(1, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGE, v.getMessage());
-        assertEquals(XPATHSTRING, v.getxPath());
-        assertEquals(7, v.getLine());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_link.bpmn"), 1);
+        assertViolation(result.getViolations().get(0), 7);
     }
 
     @Test
     public void testConstraintTimerFail() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "146" + File.separator
-                + "fail_timer.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(1, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGE, v.getMessage());
-        assertEquals(XPATHSTRING, v.getxPath());
-        assertEquals(7, v.getLine());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_timer.bpmn"), 1);
+        assertViolation(result.getViolations().get(0), 7);
     }
 
     @Test
     public void testConstraintTimerRefFail() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "146" + File.separator
-                + "fail_timer_ref.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(1, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGE, v.getMessage());
-        assertEquals(XPATHSTRING, v.getxPath());
-        assertEquals(8, v.getLine());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_timer_ref.bpmn"), 1);
+        assertViolation(result.getViolations().get(0), 8);
     }
 
     @Test
     public void testConstraintMultipleFail() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "146" + File.separator
-                + "fail_multiple.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(1, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGE, v.getMessage());
-        assertEquals(XPATHSTRING, v.getxPath());
-        assertEquals(7, v.getLine());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_multiple.bpmn"), 1);
+        assertViolation(result.getViolations().get(0), 7);
     }
 
     @Test
     public void testConstraintConditionalFail() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "146" + File.separator
-                + "fail_conditional.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(2, result.getViolations().size());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_conditional.bpmn"), 2);
         Violation v = result.getViolations().get(0);
         assertTrue(v.getMessage().contains("cvc-complex-type.2.4.b"));
         assertTrue(v.getMessage().contains("conditionalEventDefinition"));
         assertEquals("XSD-Check", v.getConstraint());
         assertEquals(9, v.getLine());
-        v = result.getViolations().get(1);
-        assertEquals(ERRORMESSAGE, v.getMessage());
-        assertEquals(XPATHSTRING, v.getxPath());
-        assertEquals(7, v.getLine());
+        assertViolation(result.getViolations().get(1), 7);
     }
 
     @Test
     public void testConstraintSuccess() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "098" + File.separator
-                + "success_message.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertTrue(result.isValid());
-        assertTrue(result.getViolations().isEmpty());
+        verifyValidResult(createFile("success.bpmn"));
     }
 
     @Test
     public void testConstraintMultipleSuccess() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "098" + File.separator
-                + "success_multiple.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertTrue(result.isValid());
-        assertTrue(result.getViolations().isEmpty());
+        verifyValidResult(createFile("success_multiple.bpmn"));
+    }
+
+    private void assertViolation(Violation v, int line) {
+        assertEquals(ERRORMESSAGE, v.getMessage());
+        assertEquals(XPATHSTRING, v.getxPath());
+        assertEquals(line, v.getLine());
+    }
+
+    @Override
+    protected String getExtNumber() {
+        return "146";
     }
 
 }
--- src/test/java/de/uniba/dsg/ppn/ba/Ext150.java
+++ src/test/java/de/uniba/dsg/ppn/ba/Ext150.java
@@ -1,102 +1,68 @@
 package de.uniba.dsg.ppn.ba;
 
-import ch.qos.logback.classic.Level;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import de.uniba.dsg.bpmn.ValidationResult;
 import de.uniba.dsg.bpmn.Violation;
 import de.uniba.dsg.ppn.ba.helper.BpmnValidationException;
-import de.uniba.dsg.ppn.ba.validation.SchematronBPMNValidator;
-import org.junit.Before;
-import org.junit.Test;
 
-import java.io.File;
+public class Ext150 extends TestCase {
 
-import static org.junit.Assert.*;
-
-public class Ext150 {
-
-    private SchematronBPMNValidator validator;
     private final static String ERRORMESSAGEONE = "If a start event is used to initiate a process, all flow nodes must have an incoming sequence flow";
     private final static String ERRORMESSAGETWO = "If end events are used, all flow nodes must have an outgoing sequence flow";
-
-    @Before
-    public void setUp() {
-        validator = new SchematronBPMNValidator();
-        validator.setLogLevel(Level.OFF);
-    }
 
     @Test
     public void testConstraintNormalSequenceFlowFail1()
             throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "fail_normal_sequence_flow_missing_1.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(2, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGEONE, v.getMessage());
-        assertEquals(
-                "//bpmn:task[@isForCompensation = 'false'] [parent::*/bpmn:startEvent][4]",
-                v.getxPath());
-        assertEquals(55, v.getLine());
-        v = result.getViolations().get(1);
-        assertEquals(ERRORMESSAGETWO, v.getMessage());
-        assertEquals(
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_normal_sequence_flow_missing_1.bpmn"), 2);
+        assertSecondViolation(
+                result.getViolations().get(1),
                 "//bpmn:subProcess[@isForCompensation = 'false' and @triggeredByEvent = 'false'] [parent::*/bpmn:endEvent][0]",
-                v.getxPath());
-        assertEquals(7, v.getLine());
+                7);
+        assertFirstViolation(
+                result.getViolations().get(0),
+                "//bpmn:task[@isForCompensation = 'false'] [parent::*/bpmn:startEvent][4]",
+                55);
     }
 
     @Test
     public void testConstraintNormalSequenceFlowFail2()
             throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "fail_normal_sequence_flow_missing_2.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(2, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGEONE, v.getMessage());
-        assertEquals(
-                "//bpmn:subProcess[@isForCompensation = 'false' and @triggeredByEvent = 'false'] [parent::*/bpmn:startEvent][0]",
-                v.getxPath());
-        assertEquals(8, v.getLine());
-        v = result.getViolations().get(1);
-        assertEquals(ERRORMESSAGETWO, v.getMessage());
-        assertEquals(
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_normal_sequence_flow_missing_2.bpmn"), 2);
+        assertSecondViolation(
+                result.getViolations().get(1),
                 "//bpmn:task[@isForCompensation = 'false'] [parent::*/bpmn:endEvent][3]",
-                v.getxPath());
-        assertEquals(49, v.getLine());
+                49);
+        assertFirstViolation(
+                result.getViolations().get(0),
+                "//bpmn:subProcess[@isForCompensation = 'false' and @triggeredByEvent = 'false'] [parent::*/bpmn:startEvent][0]",
+                8);
     }
 
     @Test
     public void testConstraintSequenceFlowInSubProcessFail1()
             throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "fail_sequence_flow_in_sub_process_missing_1.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(2, result.getViolations().size());
-        Violation v = result.getViolations().get(0);
-        assertEquals(ERRORMESSAGEONE, v.getMessage());
-        assertEquals("//bpmn:parallelGateway[parent::*/bpmn:startEvent][0]",
-                v.getxPath());
-        assertEquals(13, v.getLine());
-        v = result.getViolations().get(1);
-        assertEquals(ERRORMESSAGETWO, v.getMessage());
-        assertEquals(
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_sequence_flow_in_sub_process_missing_1.bpmn"),
+                2);
+        assertSecondViolation(
+                result.getViolations().get(1),
                 "//bpmn:task[@isForCompensation = 'false'] [parent::*/bpmn:endEvent][0]",
-                v.getxPath());
-        assertEquals(10, v.getLine());
+                10);
+        assertFirstViolation(result.getViolations().get(0),
+                "//bpmn:parallelGateway[parent::*/bpmn:startEvent][0]", 13);
     }
 
     @Test
     public void testConstraintSequenceFlowInSubProcessFail2()
             throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "fail_sequence_flow_in_sub_process_missing_2.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertFalse(result.isValid());
-        assertEquals(2, result.getViolations().size());
+        ValidationResult result = verifyInValidResult(
+                createFile("fail_sequence_flow_in_sub_process_missing_2.bpmn"),
+                2);
         Violation v = result.getViolations().get(0);
         assertEquals(
                 "A Gateway MUST have either multiple incoming Sequence Flows or multiple outgoing Sequence Flows",
@@ -104,38 +70,42 @@ public class Ext150 {
         assertEquals("//bpmn:parallelGateway[0]", v.getxPath());
         assertEquals(14, v.getLine());
         v = result.getViolations().get(1);
-        assertEquals(ERRORMESSAGEONE, v.getMessage());
-        assertEquals(
+        assertFirstViolation(
+                result.getViolations().get(1),
                 "//bpmn:callActivity[@isForCompensation = 'false'] [parent::*/bpmn:startEvent][0]",
-                v.getxPath());
-        assertEquals(38, v.getLine());
+                38);
     }
 
     @Test
     public void testConstraintSuccess() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "success.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertTrue(result.isValid());
-        assertTrue(result.getViolations().isEmpty());
+        verifyValidResult(createFile("success.bpmn"));
     }
 
     @Test
     public void testConstraintSuccess2() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "success_2.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertTrue(result.isValid());
-        assertTrue(result.getViolations().isEmpty());
+        verifyValidResult(createFile("success_2.bpmn"));
     }
 
     @Test
     public void testConstraintLinkEventSuccess() throws BpmnValidationException {
-        File f = new File(TestHelper.getTestFilePath() + "150" + File.separator
-                + "success_linkevent.bpmn");
-        ValidationResult result = validator.validate(f);
-        assertTrue(result.isValid());
-        assertTrue(result.getViolations().isEmpty());
+        verifyValidResult(createFile("success_linkevent.bpmn"));
+    }
+
+    private void assertFirstViolation(Violation v, String xpath, int line) {
+        assertEquals(ERRORMESSAGEONE, v.getMessage());
+        assertEquals(xpath, v.getxPath());
+        assertEquals(line, v.getLine());
+    }
+
+    private void assertSecondViolation(Violation v, String xpath, int line) {
+        assertEquals(ERRORMESSAGETWO, v.getMessage());
+        assertEquals(xpath, v.getxPath());
+        assertEquals(line, v.getLine());
+    }
+
+    @Override
+    protected String getExtNumber() {
+        return "150";
     }
 
 }
