d9615aad571e652528c688237cc647292205c4dc
Philipp Neugebauer <ppn@nexgo.de>
2014-11-18 14:14:36 +0100

    own subclass for ext.006 and ext.007 tests

---
+++ src/test/java/de/uniba/dsg/ppn/ba/artifacts/sequenzflow/ArtifactSequenceFlowTest.java
@@ -0,0 +1,52 @@
+package de.uniba.dsg.ppn.ba.artifacts.sequenzflow;
+
+import org.junit.Test;
+
+import de.uniba.dsg.bpmn.ValidationResult;
+import de.uniba.dsg.bpmn.Violation;
+import de.uniba.dsg.ppn.ba.TestCase;
+import de.uniba.dsg.ppn.ba.helper.BpmnValidationException;
+
+public class ArtifactSequenceFlowTest extends TestCase {
+
+    @Test
+    public void testConstraintAssociationFail() throws BpmnValidationException {
+        assertTests("Fail_association.bpmn", 11);
+
+    }
+
+    @Test
+    public void testConstraintGroupFail() throws BpmnValidationException {
+        assertTests("Fail_group.bpmn", 8);
+    }
+
+    @Test
+    public void testConstraintTextAnnotationFail()
+            throws BpmnValidationException {
+        assertTests("Fail_text_annotation.bpmn", 8);
+    }
+
+    private void assertTests(String fileName, int line)
+            throws BpmnValidationException {
+        ValidationResult result = verifyInValidResult(createFile(fileName), 3);
+        assertFirstViolation(result.getViolations().get(0), fileName);
+        assertSecondViolation(result.getViolations().get(1), fileName, line);
+        assertThirdViolation(result.getViolations().get(2), fileName);
+    }
+
+    protected void assertFirstViolation(Violation v, String fileName) {
+        throw new UnsupportedOperationException(
+                "must be overriden by every child class!");
+    }
+
+    protected void assertSecondViolation(Violation v, String fileName, int line) {
+        throw new UnsupportedOperationException(
+                "must be overriden by every child class!");
+    }
+
+    protected void assertThirdViolation(Violation v, String fileName) {
+        throw new UnsupportedOperationException(
+                "must be overriden by every child class!");
+    }
+
+}
