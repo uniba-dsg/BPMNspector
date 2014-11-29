1379430d9c389129b5d5049655d5cdaa55ace52d
Philipp Neugebauer <ppn@nexgo.de>
2014-11-29 12:57:50 +0100

    fixes execution of not wanted test classes and difference between eclipse and gradle #26

--- src/test/java/de/uniba/dsg/ppn/ba/artifacts/sequenzflow/AbstractArtifactSequenceFlowTest.java
+++ src/test/java/de/uniba/dsg/ppn/ba/artifacts/sequenzflow/AbstractArtifactSequenceFlowTest.java
@@ -6,7 +6,7 @@ import de.uniba.dsg.bpmnspector.common.Violation;
 import de.uniba.dsg.ppn.ba.TestCase;
 import de.uniba.dsg.ppn.ba.helper.BpmnValidationException;
 
-abstract class ArtifactSequenceFlowTest extends TestCase {
+abstract public class AbstractArtifactSequenceFlowTest extends TestCase {
 
     @Test
     public void testConstraintAssociationFail() throws BpmnValidationException {
