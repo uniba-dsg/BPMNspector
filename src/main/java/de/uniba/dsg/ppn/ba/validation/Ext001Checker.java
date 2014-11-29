258db691798ff6e2ea7578a116c7cef93c08029f
Philipp Neugebauer <ppn@nexgo.de>
2014-11-29 13:22:05 +0100

    classes for ext001 and ext002 check added

---
+++ src/main/java/de/uniba/dsg/ppn/ba/validation/Ext001Checker.java
@@ -0,0 +1,110 @@
+package de.uniba.dsg.ppn.ba.validation;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import ch.qos.logback.classic.Logger;
+import de.uniba.dsg.bpmnspector.common.ValidationResult;
+import de.uniba.dsg.bpmnspector.common.Violation;
+import de.uniba.dsg.bpmnspector.common.xsdvalidation.BpmnXsdValidator;
+import de.uniba.dsg.bpmnspector.common.xsdvalidation.WsdlValidator;
+import de.uniba.dsg.bpmnspector.common.xsdvalidation.XmlValidator;
+import de.uniba.dsg.ppn.ba.helper.ConstantHelper;
+import de.uniba.dsg.ppn.ba.helper.ImportedFilesCrawler;
+import de.uniba.dsg.ppn.ba.helper.LogHelper;
+import de.uniba.dsg.ppn.ba.helper.SetupHelper;
+import de.uniba.dsg.ppn.ba.preprocessing.ImportedFile;
+
+public class Ext001Checker {
+
+    private WsdlValidator wsdlValidator;
+    private XmlValidator xmlValidator;
+    private final DocumentBuilder documentBuilder;
+    private final static Logger LOGGER;
+    private final BpmnXsdValidator bpmnXsdValidator;
+    private final XmlLocator xmlLocator;
+    private static final String CONSTRAINTNUMBER = "EXT.001";
+
+    static {
+        LOGGER = (Logger) LoggerFactory.getLogger(Ext001Checker.class
+                .getSimpleName());
+    }
+
+    {
+        documentBuilder = SetupHelper.setupDocumentBuilder();
+        bpmnXsdValidator = new BpmnXsdValidator();
+        xmlLocator = new XmlLocator();
+    }
+
+    /**
+     * checks, if there are violations of the EXT.001 constraint
+     *
+     * @param headFile
+     *            the file which should be checked
+     * @param folder
+     *            the parent folder of the file
+     * @param validationResult
+     *            the current validation result of validating process for adding
+     *            found violations
+     */
+    public void checkConstraint001(File headFile, File folder,
+            ValidationResult validationResult) {
+        try {
+            bpmnXsdValidator.validateAgainstXsd(headFile, validationResult);
+            Document headFileDocument = documentBuilder.parse(headFile);
+
+            List<ImportedFile> importedFiles = ImportedFilesCrawler
+                    .selectImportedFiles(headFileDocument, folder, 0, false);
+
+            for (ImportedFile importedFile : importedFiles) {
+                checkConstraintsinFile(importedFile, headFile, folder,
+                        validationResult);
+            }
+        } catch (SAXException | IOException e) {
+            LogHelper.printLogstatements(LOGGER, e, headFile.getName());
+        }
+    }
+
+    private void checkConstraintsinFile(ImportedFile importedFile,
+            File headFile, File folder, ValidationResult validationResult)
+            throws IOException, SAXException {
+        File file = importedFile.getFile();
+        if (!file.exists()) { // NOPMD
+            String xpathLocation = createImportString(file.getName());
+            String fileName = file.getName();
+            int line = xmlLocator.findLine(headFile, xpathLocation);
+            validationResult.getViolations().add(
+                    new Violation(CONSTRAINTNUMBER, fileName, line,
+                            xpathLocation + "[0]",
+                            "The imported file does not exist"));
+            LOGGER.info("violation of constraint {} in {} found.",
+                    CONSTRAINTNUMBER, fileName);
+        } else if (ConstantHelper.BPMNNAMESPACE.equals(importedFile
+                .getImportType())) {
+            checkConstraint001(file, folder, validationResult);
+        } else if ("http://www.w3.org/TR/wsdl20/".equals(importedFile
+                .getImportType())) {
+            if (wsdlValidator == null) {
+                wsdlValidator = new WsdlValidator();
+            }
+            wsdlValidator.validateAgainstXsd(file, validationResult);
+        } else if ("http://www.w3.org/2001/XMLSchema".equals(importedFile
+                .getImportType())) {
+            if (xmlValidator == null) {
+                xmlValidator = new XmlValidator();
+            }
+            xmlValidator.validateAgainstXsd(file, validationResult);
+        }
+    }
+
+    private String createImportString(String fileName) {
+        return String.format("//bpmn:import[@location = '%s']", fileName);
+    }
+}
