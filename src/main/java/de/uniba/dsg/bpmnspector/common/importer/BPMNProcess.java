984eab02a0468e2d762795ec6b51940e1f4bc6fb
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2014-12-09 20:29:35 +0100

    Adds new commonly used ProcessImporter

---
+++ src/main/java/de/uniba/dsg/bpmnspector/common/importer/BPMNProcess.java
@@ -0,0 +1,73 @@
+package de.uniba.dsg.bpmnspector.common.importer;
+
+import org.jdom2.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BPMNProcess {
+
+    private final Document processAsDoc;
+
+    private final String baseURI;
+    private final String namespace;
+
+    private final BPMNProcess parent;
+
+    private List<BPMNProcess> children = new ArrayList<>();
+
+    private List<Document> wsdls = new ArrayList<>();
+    private List<Document> xsds = new ArrayList<>();
+
+    public BPMNProcess(Document processAsDoc, String baseURI, String namespace) {
+        this.processAsDoc = processAsDoc;
+        this.baseURI = baseURI;
+        this.namespace = namespace;
+        this.parent = null;
+    }
+
+    public BPMNProcess(Document processAsDoc, String baseURI, String namespace,
+            BPMNProcess parent) {
+        this.processAsDoc = processAsDoc;
+        this.baseURI = baseURI;
+        this.namespace = namespace;
+        this.parent = parent;
+    }
+
+    public List<BPMNProcess> getChildren() {
+        return children;
+    }
+
+    public Document getProcessAsDoc() {
+        return processAsDoc;
+    }
+
+    public String getBaseURI() {
+        return baseURI;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public BPMNProcess getParent() {
+        return parent;
+    }
+
+    public List<Document> getWsdls() {
+        return wsdls;
+    }
+
+    public List<Document> getXsds() {
+        return xsds;
+    }
+
+    public void getAllProcessesRecursively(List<BPMNProcess> processList) {
+        if(!processList.contains(this)) {
+            processList.add(this);
+            for(BPMNProcess child : this.getChildren()) {
+                child.getAllProcessesRecursively(processList);
+            }
+        }
+    }
+}
---
+++ src/main/java/de/uniba/dsg/bpmnspector/common/importer/ProcessImporter.java
@@ -0,0 +1,110 @@
+package de.uniba.dsg.bpmnspector.common.importer;
+
+import de.uniba.dsg.bpmnspector.common.ValidationResult;
+import de.uniba.dsg.bpmnspector.common.xsdvalidation.BpmnXsdValidator;
+import de.uniba.dsg.bpmnspector.refcheck.ValidatorException;
+import de.uniba.dsg.ppn.ba.helper.ConstantHelper;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.located.LocatedJDOMFactory;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ProcessImporter {
+
+    private final SAXBuilder builder;
+    private final BpmnXsdValidator bpmnXsdValidator;
+
+    public ProcessImporter() {
+        builder = new SAXBuilder();
+        builder.setJDOMFactory(new LocatedJDOMFactory());
+        bpmnXsdValidator = new BpmnXsdValidator();
+    }
+
+    public BPMNProcess importProcessFromPath(Path path, ValidationResult result)
+            throws ValidatorException {
+        return importProcessRecursively(path, null, null, result);
+    }
+
+    private BPMNProcess importProcessRecursively(Path path, BPMNProcess parent, BPMNProcess rootProcess, ValidationResult result)
+            throws ValidatorException {
+        if(Files.notExists(path) || !Files.isRegularFile(path)) {
+            throw new IllegalArgumentException("Path "+path+"is invalid.");
+        } else {
+            try {
+                bpmnXsdValidator.validateAgainstXsd(path.toFile(), result);
+                Document processAsDoc = builder.build(path.toFile());
+                String processNamespace = processAsDoc.getRootElement().getAttributeValue("targetNamespace");
+
+                BPMNProcess process = new BPMNProcess(processAsDoc, path.toString(), processNamespace ,parent);
+                if(rootProcess==null) {
+                    resolveAndAddImports(process, process, result);
+                } else {
+                    resolveAndAddImports(process, rootProcess, result);
+                }
+
+                return process;
+
+            } catch (ValidatorException e) {
+                // Thrown if file is not well-formed - error is already logged and
+                // added to the validation result - but further processing is not
+                // possible
+                return null;
+            } catch (SAXException | JDOMException | IOException e) {
+                throw new ValidatorException("Creation of BPMNProcess object failed.", e);
+            }
+        }
+    }
+
+    private void resolveAndAddImports(BPMNProcess process, BPMNProcess rootProcess, ValidationResult result)
+            throws ValidatorException {
+
+        List<Element> importElements = process.getProcessAsDoc().getRootElement().getChildren("import", getBPMNNamespace(process.getProcessAsDoc()));
+
+        for (Element elem : importElements) {
+            String importType = elem.getAttributeValue("importType");
+            if (ConstantHelper.BPMNNAMESPACE.equals(importType)) {
+                Path importPath = Paths.get(process.getBaseURI()).getParent().resolve(elem.getAttributeValue("location")).normalize().toAbsolutePath();
+
+                if(!isFileAlreadyImported(importPath.toString(), rootProcess)) {
+
+                    BPMNProcess importedProcess = importProcessRecursively(
+                            importPath, process, rootProcess, result);
+
+                    process.getChildren().add(importedProcess);
+                }
+            }
+        }
+    }
+
+    private Namespace getBPMNNamespace(Document doc) {
+        for(Namespace nsp : doc.getNamespacesInScope()) {
+            if(nsp.getURI().equals(ConstantHelper.BPMNNAMESPACE)) {
+                return nsp;
+            }
+        }
+        return null;
+    }
+
+    private boolean isFileAlreadyImported(String baseURI, BPMNProcess process) {
+        if(process.getBaseURI().equals(baseURI)) {
+            return true;
+        } else {
+            for(BPMNProcess child : process.getChildren()) {
+                if(isFileAlreadyImported(baseURI, child)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
