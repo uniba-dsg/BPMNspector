da5dcdc599ca08e0b8ba881358ead4b5be948d7b
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-05-27 09:12:39 +0200

    Add EXT018

---
+++ src/test/java/de/uniba/dsg/bpmnspector/schematron/commonExec/Ext018.java
@@ -0,0 +1,40 @@
+package de.uniba.dsg.bpmnspector.schematron.commonExec;
+
+import api.ValidationException;
+import api.ValidationResult;
+import de.uniba.dsg.bpmnspector.schematron.TestCase;
+import org.junit.Test;
+
+/**
+ * Test class for testing Constraint EXT.018
+ *
+ * @author Matthias Geiger
+ * @version 1.0
+ *
+ */
+public class Ext018 extends TestCase {
+
+    private static final String ERR_MSG = "A diverging Gateway must not have more than one incoming Sequence Flow.";
+
+    @Test
+    public void testConstraintFailExclusiveGateway() throws ValidationException {
+        ValidationResult result = verifyInvalidResult(createFile("EXT018_failure_exclusiveGateway.bpmn"), 1);
+        assertViolation(result.getViolations().get(0),
+                ERR_MSG,
+                "//bpmn:*[@gatewayDirection='Diverging'][1]", 13);
+    }
+
+    @Test
+    public void testConstraintFailParallelGateway() throws ValidationException {
+        ValidationResult result = verifyInvalidResult(createFile("EXT018_failure_parallelGateway.bpmn"), 1);
+        assertViolation(result.getViolations().get(0),
+                ERR_MSG,
+                "//bpmn:*[@gatewayDirection='Diverging'][1]", 13);
+    }
+
+
+    @Override
+    protected String getExtNumber() {
+        return "018";
+    }
+}
