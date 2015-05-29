14aa56145285f203408fb1e385c0dc6532a95a81
matthiasgeiger <matthias.geiger@uni-bamberg.de>
2015-05-29 09:52:20 +0200

    Add EXT043

---
+++ src/test/java/de/uniba/dsg/bpmnspector/schematron/commonExec/Ext043.java
@@ -0,0 +1,32 @@
+package de.uniba.dsg.bpmnspector.schematron.commonExec;
+
+import api.ValidationException;
+import api.ValidationResult;
+import de.uniba.dsg.bpmnspector.schematron.TestCase;
+import org.junit.Test;
+
+/**
+ * Test class for testing Constraint EXT.043
+ *
+ * @author Matthias Geiger
+ * @version 1.0
+ *
+ */
+public class Ext043 extends TestCase {
+
+    private static final String ERR_MSG = "If a ServiceTask references an operation, at most one OutputSet can be " +
+            "defined in the ioSpecification.";
+
+    @Test
+    public void testConstraintFail() throws ValidationException {
+        ValidationResult result = verifyInvalidResult(createFile("EXT043_failure.bpmn"), 1);
+        assertViolation(result.getViolations().get(0),
+                ERR_MSG,
+                "//bpmn:serviceTask[@operationRef][0]", 15);
+    }
+
+    @Override
+    protected String getExtNumber() {
+        return "043";
+    }
+}
